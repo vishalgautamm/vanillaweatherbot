2d33123f0b279377c5fee43c10b4e95f8f2f89b5
vishalgautamm <vishal.g@live.ca>
2017-03-08 16:12:57 -0500

    Updated the FBeamer module

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 'use strict';
 
 // Create an API server
-
 const Restify = require('restify');
 const server = Restify.createServer({
-	name: 'VanillaMessenger'
+	name: 'VanillaMessenger' // optional
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
+
+server.use(Restify.jsonp());
+server.use(Restify.bodyParser());
 
 // Tokens
 const config = require('./config');
@@ -16,12 +18,23 @@ const config = require('./config');
 const FBeamer = require('./FBeamer');
 const f = new FBeamer(config);
 
-// Test
+// Register the webhooks
 server.get('/', (req, res, next) => {
-	res.send("Hello");
+	f.registerHook(req,res);
 	return next();
 });
 
+// Receiver all incoming messages
+server.post('/', (req,res,next) => {
+	f.incoming(req,res, msg => {
+		//process individual messages
+		f.txt(msg.sender, `Hey you just said "${msg.message.text}"`);
+		f.img(msg.sender, "https://s.yimg.com/os/mit/media/m/weather/images/fallbacks/lead/cloudy_d-e618500.jpg");
+	});
+	return next();
+});
+
+// Subscribe
+f.subscribe();
+
 server.listen(PORT, () => console.log(`Vanilla running on port ${PORT}`));
-
-
